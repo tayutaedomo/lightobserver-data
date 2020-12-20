141e79a17cbb869716d52d0147cd17433e8c72d1
tayutaedomo <tayutaedomo@gmail.com>
2020-12-20 17:47:41 +0900

    Fix debug_opencv_camera.py

--- scripts/debug_opencv_camera.py
+++ scripts/debug_opencv_camera.py
@@ -6,16 +6,15 @@ import cv2
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
 
-    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    #cap.set(cv2.CAP_PROP_FPS, 5)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
     ret, frame = cap.read()
 
     height, width, channels = frame.shape[:3]
     print('width:{},height:{}'.format(width, height))
 
-    filename = 'opencv_camera_{0:%Y%m%d%H%M%S}.jpg'.format(datetime.now())
+    filename = 'opencv_{0:%Y%m%d%H%M%S}.jpg'.format(datetime.now())
     cv2.imwrite(filename, frame)
     print('Output:', filename)
 
