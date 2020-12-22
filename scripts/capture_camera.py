c8e857adb22a0f6e6aea3e5de89022ee5ffdc6d1
tayutaedomo <tayutaedomo@gmail.com>
2020-12-22 21:18:17 +0900

    Fix capture_camera.py

--- scripts/capture_camera.py
+++ scripts/capture_camera.py
@@ -7,9 +7,15 @@ ROOT_PATH = os.path.abspath(ROOT_PATH)
 sys.path.append(os.path.abspath(ROOT_PATH))
 
 from applib.camera import write_capture_image
+from applib.uploader import CaptureImageUploader
 
 
 if __name__ == '__main__':
     file_path = os.path.join(ROOT_PATH, 'tmp', 'camera.jpg')
     write_capture_image(file_path)
+    print('File:', file_path)
+
+    uploader = CaptureImageUploader()
+    gcs_path = uploader.upload()
+    print('GCS:', gcs_path)
 
