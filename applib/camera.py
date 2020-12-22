4991e86d39d3adc1ab2b5fbe1cf4be1dea01ee1b
tayutaedomo <tayutaedomo@gmail.com>
2020-12-22 20:25:38 +0900

    Add scripts/capture_camera.py

---
+++ applib/camera.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def write_capture_image(dest_path):
+    cap = cv2.VideoCapture(0)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+    ret, frame = cap.read()
+
+    cv2.imwrite(dest_path, frame)
+
+    cap.release()
+
+
+
+if __name__ == '__main__':
+    write_capture_image('a.jpg')
+
---
+++ scripts/capture_camera.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+ROOT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+ROOT_PATH = os.path.abspath(ROOT_PATH)
+sys.path.append(os.path.abspath(ROOT_PATH))
+
+from applib.camera import write_capture_image
+
+
+if __name__ == '__main__':
+    file_path = os.path.join(ROOT_PATH, 'tmp', 'camera.jpg')
+    write_capture_image(file_path)
+
